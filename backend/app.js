da4a4714362deaef2174060736af2f7229cf45a3
tzeng7 <thebosstzeng@gmail.com>
2020-10-30 22:14:36 -0500

    Add files via upload
    posts made - questions?

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,111 @@ sqlDb.connect((err) => {
 });
 
 const app = express();
-
+//Create new users
+app.post('/users', (req, res) => {
+    let id = req.body;
+    var sql = 'INSERT INTO Users SET ?';
+    sqlDb.query(sql, id, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            console.log("User data inserted");
+            res.send(result)
+        }
+    });
+});
+//Validate user 
+//fix
+app.get('/users', (req, res) => {
+    let sql = 'SELECT UserID FROM Users';
+    sqlDb.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log('Users validated');
+            res.send(result)
+        }
+    });
+});
+//Finding songs
+app.post('/findsongs', (req, res) => {
+    let id = req.body;
+    var sql = 'SELECT title FROM Songs WHERE title LIKE id';
+    sqlDb.query(sql, id, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            console.log("songs found");
+            res.send(result)
+        }
+    });
+});
+//Creating Playlist
+app.post('/createplaylist', (req, res) => {
+    let id = req.body;
+    var sql = 'INSERT INTO Playlists SET ?';
+    sqlDb.query(sql, id, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            console.log("Playlist data created");
+            res.send(result)
+        }
+    });
+});
+//Add songs to playlist
+app.post('/addtoplaylist', (req, res) => {
+    let id = req.body;
+    var sql = 'INSERT INTO SongsInPlaylist(SongID) SET ?';
+    sqlDb.query(sql, id, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            console.log("Playlist data inserted");
+            res.send(result)
+        }
+    });
+});
+//Delete Playlist
+app.post('/deleteplaylist', (req, res) => {
+    let id = req.body;
+    var sql = 'DELETE FROM Playlists, SongsInPlaylist WHERE PlaylistID = id.PlaylistID';
+    sqlDb.query(sql, id, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            console.log("Playlist data deleted");
+            res.send(result)
+        }
+    });
+});
+//Find Playlist
+app.post('/findplaylist', (req, res) => {
+    let id = req.body;
+    var sql = 'Select PlaylistID From Playlists p JOIN Users u on u.UserID = p.UserID WHERE PlaylistID = id.PlaylistID ';
+    sqlDb.query(sql, id, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            console.log("Playlist found");
+            res.send(result)
+        }
+    });
+});
+//find playlists in similar age
+app.post('/userdob', (req, res) => {
+    let id = req.body;
+    var sql = 'Select DOB FROM Users WHERE DOB >= id.DOB - 3 OR DOB <= id.DOB + 3';
+    sqlDb.query(sql, id, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            console.log("USER DOB data found");
+            res.send(result)
+        }
+    });
+});
+//question - should this be in the post route
 app.get('/viewsongs', (req, res) => {
     let sql = 'SELECT title FROM Songs';
     sqlDb.query(sql, (err, result) => {
@@ -33,7 +137,10 @@ app.get('/viewsongs', (req, res) => {
         }
     });
 });
-
+app.post('/viewsongs', (req, res) => {
+    
+    res.send('GET request to the HTTP');
+});
 app.listen('8000', () => {
     console.log('Server started on port 8000');
 });

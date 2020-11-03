c09c20a7ec00096254e4bb33567f97e6543c3b8a
jpinsky <pinsky3@illinois.edu>
2020-11-02 23:13:20 -0600

    axios working

--- frontend/src/Views/SearchSongs.jsx
+++ frontend/src/Views/SearchSongs.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import Navigation from '../Components/Navigation';
 
 function SearchSongs({userId, setUserId}) {
+    const [songs, setSongs] = useState([]);
+
+    const instance = Axios.create({
+        timeout: 1000,
+        mode: 'no-cors',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin'
+    });
+
+    useEffect(() => {
+        instance.get(`/viewsongs`).then((res) => {
+            console.log(res);
+            setSongs(res.data)
+        });
+    }, [])
+
     return (
         <div>
             <Navigation />
@@ -10,6 +30,7 @@ function SearchSongs({userId, setUserId}) {
                 placeholder='Song Name'
                 name='sngname'
                 />
+            {songs.map((title) => (<p>{title.title}</p>))}
         </div>
     );
 }

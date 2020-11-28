f48f1b962aae48cf5a1c872fc53ac60075b1ceab
im-jmop <pinskyjustin@gmail.com>
2020-11-27 22:00:08 -0600

    added mongodb gridfs routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 var bodyparser = require("body-parser");
 
 // Creating connection to SQL server
@@ -24,6 +31,65 @@ sqlDb.connect((err) => {
 const app = express();
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended:true}))
+app.use(methodOverride('__method'))
+
+const mongoURI = 'mongodb+srv://justin:pinsky@sqwa.cclup.mongodb.net/playlistplanner?retryWrites=true&w=majority'
+const mongoDb = mongoose.createConnection(mongoURI);
+
+let gfs;
+mongoDb.once('open', () => {
+    gfs = Grid(mongoDb.db, mongoose.mongo);
+    gfs.collection('AddedSongs');
+    console.log("connection made to mongodb successfully");
+});
+
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'AddedSongs'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.json({ file: req.file });
+});
+
+app.get('/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+        if (!files || files.length === 0) {
+            return res.status(404).json({
+                err: 'No files exists'
+            });
+        }
+
+        return res.json(files)
+    })
+})
+app.get('/files/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+      //return res.json(file);
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res)
+    });
+  });
 
 //Create new users
 app.post('/users', (req, res) => {
@@ -40,9 +106,7 @@ app.post('/users', (req, res) => {
         }
     });
 });
-app.get('/users', (req, res) => {
 
-});
 //Validate user 
 //fix
 app.post('/findusers', (req, res) => {
